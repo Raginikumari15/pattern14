24b6985f4874a7c7979ea8dd85de0d71f46c5915
ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-09-19 21:20:05 +0530

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   for(int i =1; i<=5; i++){
+      int k=i;
+       for (int j = 1; j<=5; j++){
+           if(j<6-i)
+               cout<<"";
+           
+           else
+           int k;
+           cout <<k;
+           k++;
+           
+       }
+      cout<<endl;
+   }
+   
+}
